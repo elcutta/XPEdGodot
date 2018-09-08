ccce282bd5e9c3365a28b1ccad3c5a40751026ab
Unknown <ariel.cattaneo@gmail.com>
2018-09-08 12:58:25 -0300

    Incr sencillo de stress y mensaje de "Game Over" cuando llega al límite

--- Juego.gd
+++ Juego.gd
@@ -3,12 +3,20 @@ extends Node
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var stressPersonaje = 0
+var MAX_STRESS = 100
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
 
-#func _process(delta):
+func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
+	$OSD/LabelStress.text = "STRESS: " + String(stressPersonaje)
+	if (stressPersonaje > MAX_STRESS):
+		$OSD/LabelGameOver.visible = true
+		pass#a a mejor vida
+	else:
+		stressPersonaje = stressPersonaje + delta
